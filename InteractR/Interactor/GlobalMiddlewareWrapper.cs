b1a8bd0b04b496bcc116371f9e12019fae9a344c
Kristoffer Olsson <kristoffer.olsson@cgi.com>
2025-04-04 13:40:34 +0200

    Release v9

--- InteractR/Interactor/GlobalMiddlewareWrapper.cs
+++ InteractR/Interactor/GlobalMiddlewareWrapper.cs
@@ -4,10 +4,16 @@ using System.Threading.Tasks;
 
 namespace InteractR.Interactor;
 
-internal sealed class GlobalMiddlewareWrapper<TUseCase, TOutputPort>(IMiddleware middleware)
+internal sealed class GlobalMiddlewareWrapper<TUseCase, TOutputPort>
     : IMiddleware<TUseCase, TOutputPort>
     where TUseCase : IUseCase<TOutputPort>
 {
+    private readonly IMiddleware _middleware;
+
+    public GlobalMiddlewareWrapper(IMiddleware middleware)
+    {
+        _middleware = middleware ?? throw new ArgumentNullException(nameof(middleware));
+    }
     public Task<UseCaseResult> Execute(TUseCase usecase, TOutputPort outputPort, Func<TUseCase, Task<UseCaseResult>> next, CancellationToken cancellationToken) 
-        => middleware.Execute(usecase, next, cancellationToken);
+        => _middleware.Execute(usecase, next, cancellationToken);
 }
